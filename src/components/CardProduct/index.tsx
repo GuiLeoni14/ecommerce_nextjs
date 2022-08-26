d0021acd3f1b78d67c8dbd3014d97184e9485cf3
GuiLeoni14 <guileone167@gmail.com>
2022-08-25 23:14:50 -0300

    adicionando produtos da api

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,19 +1,30 @@
-export function CardProduct() {
+type CardProductProps = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+};
+
+export function CardProduct(props: CardProductProps) {
     return (
         <div className="flex flex-col items-center w-full max-w-[256px] bg-neutral-100 rounded-tl-[6px] rounded-tr-[20px] rounded-bl-[20px]  rounded-br-[6px] pb-5">
-            <img src="/img/coffees/coffee_one.png" className="mt-[-20px]" />
+            <div className="max-w-[220px] w-full h-40 max-h-40 mt-[-20px] overflow-hidden ">
+                <img src={props.image} className="h-full w-full object-contain" />
+            </div>
             <div className="flex flex-wrap gap-1 mt-3">
                 <span className="inline-block px-2 text-yellow-600 bg-yellow-200 leading-[21px] rounded-full uppercase text-[10px] font-bold">
-                    Tradicional
+                    {props.category}
                 </span>
             </div>
-            <strong className="block text-gray-700 text-[20px] mt-5 mb-2 font-baloo">Expresso Tradicional</strong>
-            <span className="block text-center text-gray-400 text-[14px] w-full max-w-[216px]">
-                O tradicional café feito com água quente e grãos moídos
+            <strong className="block text-gray-700 text-[20px] mt-5 mb-2 font-baloo">{props.name}</strong>
+            <span className="block text-center text-gray-400 text-[14px] w-full max-w-[216px] max-h-[36px] text-ellipsis overflow-hidden">
+                {props.description}
             </span>
             <div className="flex justify-center items-center gap-9 mt-[33px]">
                 <span className="text-[14px]">
-                    R$ <strong className="text-[24px] font-extrabold font-baloo">9,90</strong>
+                    R$ <strong className="text-[24px] font-extrabold font-baloo">{props.price}</strong>
                 </span>
                 <div className="flex items-center  gap-2">
                     <input
--- src/templates/Home/ProductList.tsx
+++ src/templates/Home/ProductList.tsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react';
 import { CardProduct } from '../../components/CardProduct';
+import { api } from '../../services/api';
+
+type ProductData = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+};
 
 export function ProductList() {
+    const [products, setProducts] = useState<ProductData[]>([]);
+    useEffect(() => {
+        api.get<ProductData[]>('/products').then((response) => {
+            setProducts(response.data);
+        });
+    }, []);
     return (
         <div className="w-full container mx-auto pt-8">
             <h2 className="font-baloo text-[32px] font-extrabold">Nossos cafés</h2>
             <div className="flex flex-wrap items-start justify-start gap-8 mt-[54px]">
-                <CardProduct />
-                <CardProduct />
-                <CardProduct />
-                <CardProduct />
-                <CardProduct />
-                <CardProduct />
+                {products.map((product) => {
+                    return <CardProduct key={product.id} {...product} />;
+                })}
             </div>
         </div>
     );
